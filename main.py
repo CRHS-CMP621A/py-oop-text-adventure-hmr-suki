0215358fd8ec88021871d6ebd27aeab7b7ccaa08
hmr-suki <yanan2023715@gmail.com>
2026-02-11 09:42:12 -0400

    Task 1 to 5 completed

--- main.py
+++ main.py
@@ -2,4 +2,21 @@
 from room import Room
 kitchen = Room('Kitchen')
 kitchen.set_description('A dank and dirty room buzzing with files.')
-kitchen.describe()
+
+dining_hall =Room('Dining Hall')
+dining_hall.set_description('A fancy and luxury room with numerous cuisine placed on the long table.')
+
+ball_room =Room('Ball room')
+ball_room.set_description('A obsolute room where heavy dust covered the floor.')
+
+kitchen.link_room(dining_hall, 'south')
+dining_hall.link_room(kitchen,'north')
+dining_hall.link_room(ball_room,'west')
+ball_room.link_room(dining_hall,'east')
+
+current_room = kitchen
+while True:
+    print('/n')
+    current_room.get_details()
+    command = input('>')
+    current_room = current_room.move(command)
--- room.py
+++ room.py
@@ -10,9 +10,26 @@ class Room():
 
     def get_description(self):
         return self.description
-    
+
+    def get_name(self):
+        return self.name
+
     def describe(self):
         print( self.description )
 
     def link_room(self,room_to_link,direction):
         self.linked_rooms[direction] = room_to_link
+        
+     
+    def get_details(self):
+        print(self.name+': ' +self.description)
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print('The '+ room.get_name() +' is '+direction)
+
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]   
+        else:
+            print('You cannot go that way')
+            return self 
